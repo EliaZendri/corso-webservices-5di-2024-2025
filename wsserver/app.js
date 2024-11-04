1215744575e83e3b78ed498ccd188a9609946e90
EliaZendri <145663477+EliaZendri@users.noreply.github.com>
2024-11-04 14:45:48 +0000

    e

--- wsserver/app.js
+++ wsserver/app.js
@@ -15,3 +15,16 @@ const app = express();
 // Aggiungo moduli middleware alla catena di elaborazione
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+// Aggiungiamo la librearia CORS : Aggiugne nel intestazioen della Response una righa che 
+//consente ad applicazioni ospitate su altri domini di acedere al web service 
+app.use(cors());
+
+//Pubbliclo il suito web nelòla cartella chiamta public
+app.use('',express.static('Public'));
+
+// ... implemento metodi CRUD 
+
+// metto in ascolto la mia applicazione express sulla porta scielta per il WebService 4444
+const server = app.listen(4444, () => {
+    console.log('Server in ascolto sulla porta 4444 ....')
+})
