451784cbd48faa263653c9dcc2383a71b22c5d6c
EliaZendri <145663477+EliaZendri@users.noreply.github.com>
2024-11-25 15:16:05 +0000

    s

--- wsserver/aut.js
+++ wsserver/aut.js
@@ -4,6 +4,10 @@ const pool = require('./db');
 
 function administratorAuth(request, response, next){
     const autHeader = request.headers['autorization'];
-    const token = autHeader && autHeader.split('').[1];
-    
+    const token = autHeader && autHeader.split('')[1];
+    if(!token) {
+        return response.status(401).json({
+            messaggio: 'accesso non autorizato',
+        })
+    }
 }
